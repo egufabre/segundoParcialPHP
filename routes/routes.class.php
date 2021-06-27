e33a1f3e25a77c2fdfbe9e8096636289f6f85b58
jesuslautaro <jesus.lautaro99@gmail.com>
2021-06-26 22:01:37 -0300

    Cambios varios

--- routes/routes.class.php
+++ routes/routes.class.php
@@ -1,1 +1,39 @@
 <?php
+      require '../utils/autoloader.php';
+
+      class Routes {
+          private static $routes = Array();
+          private static $notFound;
+  
+          public static function Add($url,$funcion,$metodo = "get"){
+              array_push(self::$routes,[
+                  'url' => $url,
+                  'funcion' => $funcion,
+                  'metodo' => $metodo
+              ]);
+          }
+  
+          public static function Run(){
+              $urlNavegador = $_SERVER['REQUEST_URI'];
+              $metodoNavegador = strtolower($_SERVER['REQUEST_METHOD']);
+  
+              self::$notFound = true;
+              
+              foreach(self::$routes as $route){
+                  if($urlNavegador === $route['url'] && $metodoNavegador === $route['metodo']){
+                     
+                      self::$notFound = false;
+                      break;
+                  }
+              }
+  
+              if(self::$notFound){
+                  cargarVista("404");
+              }
+  
+  
+  
+  
+          }
+      }
+  
