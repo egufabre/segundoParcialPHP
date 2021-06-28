af73fd73870648cc3cc7747e1fe04105a1e8e103
Eduardo Fabre <egufabre@hotmail.com>
2021-06-27 22:32:08 -0300

    se roba html y se borran algunas cositas que no necesitamos aun no funca css

---
+++ vistas/index.php
@@ -0,0 +1,48 @@
+
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="verPuertos" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Nuestro "Blog-Sito"</title>
+    <link rel="stylesheet" href="public/css/estilos.css">    
+  </head>
+
+  <body>
+  <nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top px-5 bg-dark">
+  <a class="navbar-brand d-inline-block" href="#"> Nuestro "Blog-Sito"<p id="version">Para vos Gonzaaaa</p></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse navbarCollapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo constant('URL'); 
+        ?>inicio">Inicio<span class="sr-only">(current)</span></a>
+      </li>     
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo constant('URL'); 
+        ?>iniciarSesion">Iniciar Sesion</a>
+      </li>
+    </ul>
+
+  </div>  
+</nav><br><br><br><br>
+
+<section>
+    <article> </article>
+</section>
+
+<aside>
+    <div id="sidebar"> 
+    <div class="title">Historial de publicaciones
+    </div>
+        <ul>
+            <li class="Julio">Julio</li>
+        </ul>       
+  </div>
+</aside>
+
+  </body>
+</html>
