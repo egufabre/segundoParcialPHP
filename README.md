cf16eff25646630c28211f0db037dc3c58d7240a
Eduardo Fabre <egufabre@hotmail.com>
2021-06-30 00:02:14 -0300

    se crea AutorModelo y la funciones para insertar un autor

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 CREAR ESTA BASE DE DATOS:
+use base;
 
+create table autor(
+    id int primary key auto_increment,
+    nombre varchar(20),
+    apellido varchar(20),
+    mail varchar(30),
+    password varchar(50),
+    foto blob
+);
---
+++ modelos/AutorModelo.class.php
@@ -0,0 +1,36 @@
+<?php
+    require "../utils/autoloader.php";
+
+    class AutorModelo extends Modelo{
+        public $id;
+        public $nombre;
+        public $apellido;
+        public $mail;
+        public $password;
+        public $foto;
+
+        private function hashearPass($password){
+            return password_hash($password, PASSWORD_DEFAULT);
+        }
+        private function insertarAutor(){
+            $this -> password = $this -> hashearPass($this -> password);
+            $sql = "INSERT INTO autor(nombre, apellido, mail, password, foto) VALUES (?,?,?,?,?)";
+            $this -> sentencia = $this -> conexion -> prepare($sql);
+            $this -> sentencia -> bind_params("ssssb",
+                $this -> nombre,
+                $this -> apellido,
+                $this -> mail,
+                $this -> password,
+                $this -> foto,
+        );
+        }
+
+        public function Guardar(){
+            $this -> insertarAutor();
+            $this -> sentencia -> execute();
+            if($this -> sentencia -> error){
+                throw new Exception("Hubo un problema al insertar un nuevo autor: " . $this -> sentencia -> error);
+            }
+        }
+
+    }
